96a68498d87058faba180e28b514933e5e4b6734
Javante-web <javanteimagery@gmail.com>
2025-04-07 08:18:07 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('#mobile-menu')
+const menuLinks = documents.querySelector('.navbar_menu')
+
+menu.addEventListener('click',function() {
+    menu.classList.toggle('is-active');
+    menuLinks.classLists.toggle('active');
+})
